80a297861cb68852a752554307e61cbf2ae1a390
NinjaPro93 <ninjapro93lu@gmail.com>
2024-09-11 22:27:24 -0300

    agr o random ta certo

--- main.asm
+++ main.asm
@@ -23,20 +23,14 @@ main:
     add s0, zero, a0 # s0 = num_usuario
     
     #gerando numero aleatorio
-    addi a7, zero, 41
+    addi a7, zero, 42
+    addi a1, zero, 3
     ecall
 
     add s1, zero, a0 # s1 = random_num //GIGANTE
     
-    #arrumando pra random_num e {-1,0,1}
-    addi t1, zero, 3 
-    div t0, s1, t1
-    mul t0, t1, t0
-
-    sub s2, s1, t0 #s2 = random_num{-1,0,1}
-
     addi a7, zero, 1
-    add a0, zero, s2
+    add a0, zero, s1
     ecall
 
     addi a7, zero, 10
