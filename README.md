e2fa975d0bf17d2903b056d3872a826234227153
NinjaPro93 <ninjapro93lu@gmail.com>
2024-09-11 22:23:10 -0300

    peguei o numero do usuario e gerei um random

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # riscV-Assembly
+
+rars system calls https://github.com/TheThirdOne/rars/wiki/Environment-Calls
+rars instruction  https://github.com/TheThirdOne/rars/wiki/Supported-Instructions
--- main.asm
+++ main.asm
@@ -1,10 +1,10 @@
     .data
     .align 0
 
-str_beg: .asciz "Escolha uma opção: \n0. Encerrar\n1. pedra\n2. papel\n3. tesoura"
+str_beg: .asciz "Escolha uma opção: \n0. Encerrar\n1. pedra\n2. papel\n3. tesoura\n\n"
 
-str_w: .asciz "Voce Venceu!"
-str_l: .asciz "Voce Perdeu!"
+str_win: .asciz "Voce Venceu!"
+str_los: .asciz "Voce Perdeu!"
 
     .text
     .align 2
@@ -16,7 +16,28 @@ main:
     la a0, str_beg
     ecall
 
+    #lendo numero do usuario
+    addi a7, zero, 5
+    ecall
+
+    add s0, zero, a0 # s0 = num_usuario
     
+    #gerando numero aleatorio
+    addi a7, zero, 41
+    ecall
+
+    add s1, zero, a0 # s1 = random_num //GIGANTE
+    
+    #arrumando pra random_num e {-1,0,1}
+    addi t1, zero, 3 
+    div t0, s1, t1
+    mul t0, t1, t0
+
+    sub s2, s1, t0 #s2 = random_num{-1,0,1}
+
+    addi a7, zero, 1
+    add a0, zero, s2
+    ecall
 
     addi a7, zero, 10
     ecall
